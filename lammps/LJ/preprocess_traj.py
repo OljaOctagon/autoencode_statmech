82e40baf0452fdfc3c422b51406270b4baa801fc
oljaoctagon <oljaoctagon@github.com>
2025-11-30 20:14:02 +0100

    preprocess added, before testing

--- lammps/LJ/preprocess_traj.py
+++ lammps/LJ/preprocess_traj.py
@@ -1,10 +1,43 @@
 import keras
 import numpy as np
 import pandas as pd
-from tools import distances, neighbours, read_traj, nextN_neighbours
+from lammpstools import distances, neighbours, read_traj, pair_distances
 from scipy.spatial.distance import squareform
+import argparse 
 
+parser = argparse.ArgumentParser 
+parser.add_argument(-n, dtype=int)
+parser.parse_args()
 
+N_pick = int(parser.n)
+Min_distance = 5 
 Natoms, Config, Box = read_traj("trajectory.lammpstrj")
-ti = 0
-dist = distances(Config[ti] * Box[ti], Box[ti])
+#N_steps = len(Config)
+N_steps = 1 
+all_dists = []
+for ti in range(N_steps):
+    # first pick particle at random
+    id=np.random.randint(1,Natoms+1)
+    # initiate list of picked particles
+    picked = [id]
+    while len(picked)<N_pick:
+        #pick next particle at random 
+        id_candidate = np.random.randint(1,Natoms+1)
+        dist_ij = 1000 
+        if id_candidate not in picked:
+            for id_check in picked:
+                dist_ij = pair_distances(id_check, id_candidate,Config[ti]*Box[ti], Box[ti])
+                if dist_ij < Min_distance:
+                    break
+            # check if all picked ids were checked and if yes:
+            # add candidate to picked ids 
+            if id_check == picked[-1]:
+                picked.append(id_candidate)
+    
+print(picked)
+                
+            
+
+
+
+
